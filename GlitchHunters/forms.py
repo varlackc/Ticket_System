93ccca0dc6d47e93824888d11686a08c59fb1e2a
bennyalash <108185043+bennyalash@users.noreply.github.com>
2023-04-08 16:00:41 -0400

    Forms.py

---
+++ GlitchHunters/forms.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()], )
+    password = PasswordField('Password', validators=[DataRequired()])
+    # remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
